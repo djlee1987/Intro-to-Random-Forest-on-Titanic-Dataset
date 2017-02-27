8cd495ac35ef11d95d6ce5b3bfbba87977d2f181
djlee1987 <leedanjun@gmail.com>
2017-02-27 00:12:49 -0500

    Random Forest with Titanic Dataset
    UIS Data Science Essentials - WK7 Exercise

---
+++ Random_Forest_on_Titanic_Dataset.py
@@ -0,0 +1,249 @@
+
+# coding: utf-8
+
+# In[1]:
+
+get_ipython().magic('matplotlib inline')
+
+
+# In[2]:
+
+# With the goal above, I will import just what I need. 
+# The model to use (I already imported it above, but will do it again here so each example is self-contained)
+from sklearn.ensemble import RandomForestRegressor
+
+# The error metric. In this case, we will use c-stat (aka ROC/AUC)
+from sklearn.metrics import roc_auc_score
+
+# An efficient data structure. 
+import pandas as pd
+
+# Import the data
+X = pd.read_csv("train.csv")
+y = X.pop("Survived")
+
+
+# In[3]:
+
+X.describe()
+
+
+# In[4]:
+
+# regress Age on SibSp and Parch
+from sklearn.linear_model import LinearRegression
+A = pd.DataFrame()
+A['Age'] = X['Age']
+A['SibSp'] = X['SibSp']
+A['Parch'] = X['Parch']
+A_dropped = A.dropna(axis = 0)
+b = A_dropped['Age']
+A_dropped = A_dropped.drop(['Age'], axis = 1)
+clf = LinearRegression()
+clf.fit(A_dropped, b)
+newAge = pd.DataFrame()
+coefficients = clf.coef_
+intercept = clf.intercept_
+#avgAge = df.Age.mean()
+#df.Age = df.Age.fillna(value = avgAge)
+
+
+# In[5]:
+
+# replace null Age values with regression estimated results
+for i in range(0, A.shape[0]):
+    if pd.isnull(A.Age[i]):
+        A.iloc[i,0] = intercept + (coefficients[0]*A.SibSp[i]) + (coefficients[1]*A.Parch[i])
+X.Age = A.Age # insert new Age Column Values into X
+
+
+# In[6]:
+
+X.describe()
+
+
+# In[7]:
+
+# Here is a simple function to show descriptive stats on the categorical variables
+def describe_categorical(X):
+    """
+    Just like .describe(), but returns the results for
+    categorical variables only.
+    """
+    from IPython.display import display, HTML
+    display(HTML(X[X.columns[X.dtypes == "object"]].describe().to_html()))
+    
+describe_categorical(X)
+
+
+# In[8]:
+
+# Drop the variables I don't feel like dealing with for this tutorial
+X.drop(["Name", "Ticket", "PassengerId"], axis=1, inplace=True)
+
+
+# In[9]:
+
+# Change the Cabin variable to be only the first letter or None
+def clean_cabin(x):
+    try:
+        return x[0]
+    except TypeError:
+        return "None"
+
+X["Cabin"] = X.Cabin.apply(clean_cabin)
+
+
+# In[10]:
+
+categorical_variables = ['Sex', 'Cabin', 'Embarked']
+
+for variable in categorical_variables:
+    # Fill missing data with the word "Missing"
+    X[variable].fillna("Missing", inplace=True)
+    # Create array of dummies
+    dummies = pd.get_dummies(X[variable], prefix=variable)
+    # Update X to include dummies and drop the main variable
+    X = pd.concat([X, dummies], axis=1)
+    X.drop([variable], axis=1, inplace=True)
+
+
+# In[11]:
+
+X
+
+
+# In[12]:
+
+model = RandomForestRegressor(100, oob_score=True, n_jobs=-1, random_state=42)
+model.fit(X, y)
+print ("Baseline C-stat: ", roc_auc_score(y, model.oob_prediction_))
+
+
+# In[13]:
+
+# Complex version that shows the summary view
+
+def graph_feature_importances(model, feature_names, autoscale=True, headroom=0.05, width=10, summarized_columns=None):
+    """
+    By Mike Bernico
+    
+    Graphs the feature importances of a random decision forest using a horizontal bar chart. 
+    Probably works but untested on other sklearn.ensembles.
+    
+    Parameters
+    ----------
+    ensemble = Name of the ensemble whose features you would like graphed.
+    feature_names = A list of the names of those featurs, displayed on the Y axis.
+    autoscale = True (Automatically adjust the X axis size to the largest feature +.headroom) / False = scale from 0 to 1
+    headroom = used with autoscale, .05 default
+    width=figure width in inches
+    summarized_columns = a list of column prefixes to summarize on, for dummy variables (e.g. ["day_"] would summarize all day_ vars
+    """
+    
+    if autoscale:
+        x_scale = model.feature_importances_.max()+ headroom
+    else:
+        x_scale = 1
+    
+    feature_dict=dict(zip(feature_names, model.feature_importances_))
+    
+    if summarized_columns: 
+        #some dummy columns need to be summarized
+        for col_name in summarized_columns: 
+            #sum all the features that contain col_name, store in temp sum_value
+            sum_value = sum(x for i, x in feature_dict.items() if col_name in i )  
+            
+            #now remove all keys that are part of col_name
+            keys_to_remove = [i for i in feature_dict.keys() if col_name in i ]
+            for i in keys_to_remove:
+                feature_dict.pop(i)
+            #lastly, read the summarized field
+            feature_dict[col_name] = sum_value
+        
+    results = pd.Series(feature_dict)
+    results.sort_values(inplace=True)
+    results.plot(kind="barh", figsize=(width,len(results)/4), xlim=(0,x_scale))
+    
+graph_feature_importances(model, X.columns, summarized_columns=categorical_variables)
+
+
+# In[14]:
+
+results = []
+n_estimator_options = [30, 50, 100, 200, 500, 1000, 2000]
+
+for trees in n_estimator_options:
+    model = RandomForestRegressor(trees, oob_score=True, n_jobs=-1, random_state=42)
+    model.fit(X, y)
+    print (trees, "trees")
+    roc = roc_auc_score(y, model.oob_prediction_)
+    print ("C-stat: ", roc)
+    results.append(roc)
+    print ("")
+    
+pd.Series(results, n_estimator_options).plot();
+
+
+# In[15]:
+
+results = []
+max_features_options = ["auto", None, "sqrt", "log2", 0.9, 0.2]
+
+for max_features in max_features_options:
+    model = RandomForestRegressor(n_estimators=1000, oob_score=True, n_jobs=-1, random_state=42, max_features=max_features)
+    model.fit(X, y)
+    print (max_features, "option")
+    roc = roc_auc_score(y, model.oob_prediction_)
+    print ("C-stat: ", roc)
+    results.append(roc)
+    print ("")
+    
+pd.Series(results, max_features_options).plot(kind="barh", xlim=(.85,.88));
+
+
+# In[16]:
+
+results = []
+min_samples_leaf_options = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for min_samples in min_samples_leaf_options:
+    model = RandomForestRegressor(n_estimators=1000, 
+                                  oob_score=True, 
+                                  n_jobs=-1, 
+                                  random_state=42, 
+                                  max_features="auto", 
+                                  min_samples_leaf=min_samples)
+    model.fit(X, y)
+    print (min_samples, "min samples")
+    roc = roc_auc_score(y, model.oob_prediction_)
+    print ("C-stat: ", roc)
+    results.append(roc)
+    print ("")
+    
+pd.Series(results, min_samples_leaf_options).plot();
+
+
+# In[17]:
+
+model = RandomForestRegressor(n_estimators=1000, 
+                              oob_score=True, 
+                              n_jobs=-1, 
+                              random_state=42, 
+                              max_features=0.9, 
+                              min_samples_leaf=7)
+model.fit(X, y)
+roc = roc_auc_score(y, model.oob_prediction_)
+print ("Final C-stat: ", roc)
+
+
+# ### Final Model with C-stat of 0.875637789069
+# > Taking Jason's Model as a baseline
+# >+ Impute Age with predicted value from Regressing Age on SibSP and Parch
+# >+ max_features = 0.9
+# >+ min_samples_leaf = 7
+
+# In[ ]:
+
+
+
